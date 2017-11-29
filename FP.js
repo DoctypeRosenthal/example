c4cd107f4b36700dfeb3986ca700117d8523aca6
Lorenz Rosenthal <lorenz.rosenthal@gmail.com>
2017-11-29 19:48:10 +0100

    Create FP.js

---
+++ FP.js
@@ -0,0 +1,12 @@
+function multMatrices(A, B) {
+		if (!A || !B || !A[0] || (A[0].length !== B.length)) {
+			// matrix A has less or more cols than matrix B has rows
+			// -> multiplication is not defined!
+			return
+		}
+		// multiplication is defined!
+		let colsB = B[0].map((y,i) => B.map(x => x[i])) // get col-vectors of B
+		// Multiply every element of a row in A with the element at the same index in colB. Then add up.
+		// Do that for every col in B. Then go to next row in A.
+		return A.map(rowA => colsB.map(colB => colB.map((x, i) => x*rowA[i]).reduce((a, b) => a+b, 0)))
+}
